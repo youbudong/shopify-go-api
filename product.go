8a6f73c9560c28a989cde3eb8bd363ec9648a86e
Luther Monson <luther.monson@gmail.com>
2020-02-02 10:00:03 -0700

    adding product list options (#78)

--- product.go
+++ product.go
@@ -62,6 +62,18 @@ type ProductOption struct {
 	Values    []string `json:"values,omitempty"`
 }
 
+type ProductListOptions struct {
+	ListOptions
+	CollectionID          int64     `url:"collection_id,omitempty"`
+	ProductType           string    `url:"product_type,omitempty"`
+	Vendor                string    `url:"vendor,omitempty"`
+	Handle                string    `url:"handle,omitempty"`
+	PublishedAtMin        time.Time `url:"published_at_min,omitempty"`
+	PublishedAtMax        time.Time `url:"published_at_max,omitempty"`
+	PublishedStatus       string    `url:"published_status,omitempty"`
+	PresentmentCurrencies string    `url:"presentment_currencies,omitempty"`
+}
+
 // Represents the result from the products/X.json endpoint
 type ProductResource struct {
 	Product *Product `json:"product"`
