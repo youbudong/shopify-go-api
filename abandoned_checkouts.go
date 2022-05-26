136d7c968074bedee6a9b1207069b84485ce129c
alanal <alanal@qq.com>
2022-05-26 23:46:48 +0800

    0

--- abandoned_checkouts.go
+++ abandoned_checkouts.go
@@ -25,46 +25,45 @@ type AbandonedCheckoutsServiceOp struct {
 
 // AbandonedCheckouts represents a shopify draft order
 type AbandonedCheckouts struct {
-	ID                       int64           `json:"id,omitempty"`
-	AbandonedCheckoutUrl     string          `json:"abandoned_checkout_url"`
-	BillingAddress           *Address        `json:"billing_address,omitempty"`
-	BuyerAcceptsMarketing    bool            `json:"buyer_accepts_marketing"`
-	BuyerAcceptsSmsMarketing bool            `json:"buyer_accepts_sms_marketing"`
-	CartToken                string          `json:"cart_token"`
-	Customer                 *Customer       `json:"customer,omitempty"`
-	CustomerLocale           string          `json:"customer_locale,omitempty"`
-	DeviceID                 int64           `json:"device_id,omitempty"`
-	DiscountCodes            []DiscountCode  `json:"discount_codes,omitempty"`
-	Email                    string          `json:"email,omitempty"`
-	Gateway                  string          `json:"gateway,omitempty"`
-	LandingSite              string          `json:"landing_site,omitempty"`
-	LineItems                []LineItem      `json:"line_items,omitempty"`
-	LocationId               int64           `json:"location_id,omitempty"`
-	Note                     string          `json:"note,omitempty"`
-	Phone                    string          `json:"phone,omitempty"`
-	ReferringSite            string          `json:"referring_site,omitempty"`
-	ShippingAddress          *Address        `json:"shipping_address,omitempty"`
-	ShippingLines            []ShippingLines `json:"shipping_lines,omitempty"`
-	SourceName               string          `json:"source_name,omitempty"`
-	SubtotalPrice            string          `json:"subtotal_price,omitempty"`
-	TaxLines                 []TaxLine       `json:"tax_lines,omitempty"`
-	TaxesIncluded            bool            `json:"taxes_included,omitempty"`
-	Token                    string          `json:"token,omitempty"`
-	TotalDiscounts           string          `json:"total_discounts,omitempty"`
-	TotalLineItemsPrice      string          `json:"total_line_items_price,omitempty"`
-	TotalPrice               string          `json:"total_price,omitempty"`
-	TotalTax                 string          `json:"total_tax,omitempty"`
-	TotalWeight              int             `json:"total_weight,omitempty"`
-	UserId                   int64           `json:"user_id,omitempty"`
-	Currency                 string          `json:"currency,omitempty"`
-	ClosedAt                 *time.Time      `json:"closed_at,omitempty"`
-	CompletedAt              *time.Time      `json:"completed_at,omitempty"`
-	CreatedAt                *time.Time      `json:"created_at,omitempty"`
-	UpdatedAt                *time.Time      `json:"updated_at,omitempty"`
+	ID                       int64          `json:"id,omitempty"`
+	AbandonedCheckoutUrl     string         `json:"abandoned_checkout_url"`
+	BillingAddress           *Address       `json:"billing_address,omitempty"`
+	BuyerAcceptsMarketing    bool           `json:"buyer_accepts_marketing"`
+	BuyerAcceptsSmsMarketing bool           `json:"buyer_accepts_sms_marketing"`
+	CartToken                string         `json:"cart_token"`
+	Customer                 *Customer      `json:"customer,omitempty"`
+	CustomerLocale           string         `json:"customer_locale,omitempty"`
+	DeviceID                 int64          `json:"device_id,omitempty"`
+	DiscountCodes            []DiscountCode `json:"discount_codes,omitempty"`
+	Email                    string         `json:"email,omitempty"`
+	Gateway                  string         `json:"gateway,omitempty"`
+	LandingSite              string         `json:"landing_site,omitempty"`
+	LineItems                []LineItem     `json:"line_items,omitempty"`
+	LocationId               int64          `json:"location_id,omitempty"`
+	Note                     string         `json:"note,omitempty"`
+	Phone                    string         `json:"phone,omitempty"`
+	ReferringSite            string         `json:"referring_site,omitempty"`
+	ShippingAddress          *Address       `json:"shipping_address,omitempty"`
+	SourceName               string         `json:"source_name,omitempty"`
+	SubtotalPrice            string         `json:"subtotal_price,omitempty"`
+	TaxLines                 []TaxLine      `json:"tax_lines,omitempty"`
+	TaxesIncluded            bool           `json:"taxes_included,omitempty"`
+	Token                    string         `json:"token,omitempty"`
+	TotalDiscounts           string         `json:"total_discounts,omitempty"`
+	TotalLineItemsPrice      string         `json:"total_line_items_price,omitempty"`
+	TotalPrice               string         `json:"total_price,omitempty"`
+	TotalTax                 string         `json:"total_tax,omitempty"`
+	TotalWeight              int            `json:"total_weight,omitempty"`
+	UserId                   int64          `json:"user_id,omitempty"`
+	Currency                 string         `json:"currency,omitempty"`
+	ClosedAt                 *time.Time     `json:"closed_at,omitempty"`
+	CompletedAt              *time.Time     `json:"completed_at,omitempty"`
+	CreatedAt                *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt                *time.Time     `json:"updated_at,omitempty"`
 }
 
 type AbandonedCheckoutsesResource struct {
-	AbandonedCheckoutses []AbandonedCheckouts `json:"abandoned_checkoutses"`
+	AbandonedCheckoutses []AbandonedCheckouts `json:"checkouts"`
 }
 
 // AbandonedCheckoutsListOptions represents the possible options that can be used
@@ -83,6 +82,7 @@ type AbandonedCheckoutsListOptions struct {
 func (s *AbandonedCheckoutsServiceOp) List(options interface{}) ([]AbandonedCheckouts, error) {
 	path := fmt.Sprintf("%s.json", AbandonedCheckoutsBasePath)
 	resource := new(AbandonedCheckoutsesResource)
+	fmt.Println(path)
 	err := s.client.Get(path, resource, options)
 	return resource.AbandonedCheckoutses, err
 }
